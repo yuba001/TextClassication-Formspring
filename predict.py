0987bf9ac917f2e79661390e177d6bc252425bf5
yuba001 <ybhan001@gmail.com>
2017-10-13 19:36:32 -0400

    included expression for precision, recall

--- predict.py
+++ predict.py
@@ -4,10 +4,12 @@ import pickle
 import numpy
 numpy.random.seed(42)
 
+PERF_FORMAT_STRING = "\
+\tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\t\
+Recall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
+RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\
+\tFalse negatives: {:4d}\t True negatives: {:4d}"
 
-### The words (features) and authors (labels), already largely processed.
-### These files should have been created from the previous (Lesson 10)
-### mini-project.
 words_file = "my_dataset.pkl"
 labels_file = "my_feature_list.pkl"
 word_data = pickle.load( open(words_file, "rb"))
@@ -38,6 +40,7 @@ labels_test = labels_test[:2]
 """
 #print(features_train[:5])
 ### your code goes here
+from sklearn.naive_bayes import GaussianNB
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
@@ -48,14 +51,52 @@ features_test  = vectorizer.transform(features_test)
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 clf = tree.DecisionTreeClassifier()
+#clf = GaussianNB()
 clf = clf.fit(features_train, labels_train)
 pred =  clf.predict(features_test)
-accuracy = accuracy_score(pred,labels_test)
-print("accuracy is: ", round(accuracy,3))
+#accuracy = accuracy_score(pred,labels_test)
+#print("accuracy is: ", round(accuracy,3))
+#[print(type(int(item))) for item in pred]
 
-for rank in range(len(clf.feature_importances_)):
+true_negatives = 0
+false_negatives = 0
+true_positives = 0
+false_positives = 0
+for prediction, truth in zip(pred, labels_test):
+    prediction, truth = int(prediction), int(truth)
+    if prediction == 0 and truth == 0:
+        true_negatives += 1
+    elif prediction == 0 and truth == 1:
+        false_negatives += 1
+    elif prediction == 1 and truth == 0:
+        false_positives += 1
+    elif prediction == 1 and truth == 1:
+        true_positives += 1
+    else:
+        print("Warning: Found a predicted label not == 0 or 1.")
+        print("All predictions should take value 0 or 1.")
+        print("Evaluating performance for processed predictions:")
+        break
+try:
+    total_predictions = true_negatives + false_negatives + false_positives + true_positives
+    accuracy = 1.0*(true_positives + true_negatives)/total_predictions
+    precision = 1.0*true_positives/(true_positives+false_positives)
+    recall = 1.0*true_positives/(true_positives+false_negatives)
+    f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
+    f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
+    print(clf)
+    print(PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision = 5))
+    print(RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives))
+    print("")
+except:
+    print("Got a divide by zero when trying out:", clf)
+    print("Precision or recall may be undefined due to a lack of true positive predicitons.")
+
+
+
+#for rank in range(len(clf.feature_importances_)):
     #if clf.feature_importances_[rank] > 0.2:
     #print(rank, clf.feature_importances_[rank])
-    pass
-print(vectorizer.get_feature_names()[5591])
-print(vectorizer.get_feature_names()[5752])
+#    pass
+#print(vectorizer.get_feature_names()[5591])
+#print(vectorizer.get_feature_names()[5752])
